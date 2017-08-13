36e8d1b8d1f590440a30f766518f400c14237d85
JustAGod1 <shuval2111@gmail.com>
2017-08-13 15:06:16 +0300

    Proxy

--- src/main/java/com/justagod/shadowcraft/network/ClientProxy.java
+++ src/main/java/com/justagod/shadowcraft/network/ClientProxy.java
@@ -53,11 +53,7 @@ public class ClientProxy extends CommonProxy {
 
     @Override
     public EntityPlayer getPlayerEntity(MessageContext ctx) {
-        // Note that if you simply return 'Minecraft.getMinecraft().thePlayer',
-        // your packets will not work because you will be getting a client
-        // player even when you are on the server! Sounds absurd, but it's true.
 
-        // Solution is to double-check side before returning the player:
         return (ctx.side.isClient() ? Minecraft.getMinecraft().thePlayer : super.getPlayerEntity(ctx));
     }
 
